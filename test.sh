9fbac6982574e628bc103ebba9102b80262fa358
Mateusz Dudzinski <mateuszpd7@gmail.com>
2018-03-30 13:07:25 +0200

    Major work with a testing script.

--- test.sh
+++ test.sh
@@ -8,20 +8,18 @@ PROGRAM=./bin/program
 
 CHECK_FOR_MEM_LEAKS=true
 
-printGood() {
-    echo -e "\e[1;32m$1\e[0m"
-}
+MAX_ALLOCATED_SIZE=0
+MAX_TIME=0.0
 
-printBad() {
-    echo -e "\e[1;31m$1\e[0m"
-}
+PROGRAM_OUT=`mktemp`
+PROGRAM_ERR=`mktemp`
+PROGRAM_VALG=`mktemp`
 
 yesNoConfirm() {
-    if [ $# -eq 0 ]; then PROMPT="Continue?"; else PROMPT=$1; fi
-
-    read -p "$PROMPT [Y/n]? " CONT
-    if [ "$CONT" = "n" ] || [ "$CONT" = "N" ] \
-           || [ "$CONT" = "no" ] || [ "$CONT" = "No" ]; then
+    read -s -n1 -p "$1 [Y/n]?" USER_INPUT
+    echo ""
+    if [ "$USER_INPUT" = "n" ] || [ "$USER_INPUT" = "N" ] \
+           || [ "$USER_INPUT" = "no" ] || [ "$USER_INPUT" = "No" ]; then
         false
     else
         true
@@ -45,47 +43,63 @@ for i in $(ls $DIRECTORY | egrep -i '*.in'); do
     OUTPUT=$DIRECTORY/${i%in}out
     ERR=$DIRECTORY/${i%in}err
 
-    echo -e "Doing test on file: $INPUT"
+    echo -e "Doing test on file: \e[1m$INPUT\e[0m"
 
     # Check if correspoing .out and .err files exists.
     if [ ! -f $OUTPUT ] || [ ! -f $ERR ]; then
         # TODO: Message about no corresponding files!
-        echo -e "No corresponding files for test: $INPUT! Skipping..."
+        echo "No corresponding files for test: $INPUT! Skipping..."
         continue
     fi
 
-    # Scripting test makes two temporary files my_out.out and my_out.err to
-    # store program output.
-    $PROGRAM < $INPUT > my_out.out 2> my_out.err
+    # TODO: Also it makes valgr output. Add info about it! Scripting test makes
+    # two temporary files $PROGRAM_OUT and $PROGRAM_ERR to store program output.
+
+    TIME_START=$(date +%s.%N)
+    $PROGRAM < $INPUT > $PROGRAM_OUT 2> $PROGRAM_ERR
     PROGRAM_EXIT_CODE=$?
+    TIME_END=$(date +%s.%N)
+    TIME_DIFF=$(echo "$TIME_END - $TIME_START" | bc | awk '{printf "%f", $0}')
+
+    # bash dont do great job when it comes to compare floating point numbers...
+    if (( `echo "$TIME_DIFF > $MAX_TIME" | bc` )); then
+        MAX_TIME="$TIME_DIFF"
+        MAX_TIME_FILE=$INPUT
+    fi
 
     if [ "$PROGRAM_EXIT_CODE" -ne "0" ]; then
-        echo "Program execution failed with error code: $PROGRAM_EXIT_CODE"
+        echo -n "Program execution failed with error code: "
+        echo -e "\e[1;31m$PROGRAM_EXIT_CODE\e[0m"
+        # TODO: Continue testing?
         continue
+    else
+        echo -e "Program execution            \e[1;32mOK\e[0m (Time: ${TIME_DIFF} s)"
     fi
 
     # TODO: Collapse differs!!!
     echo -n "Comparing standard output... "
-    if diff my_out.out $OUTPUT &> /dev/null; then
-        printGood "OK"
+    if diff $PROGRAM_OUT $OUTPUT &> /dev/null; then
+        echo -e "\e[1;32mOK\e[0m"
     else
-        printBad "DIFFERS!"
+        echo -e "\e[1;31mDIFFERS!\e[0m"
         if yesNoConfirm "Show diff?"; then
-            diff --color my_out.out $OUTPUT
-            if yesNoConfirm "Abort testing?"; then
+            diff --color $PROGRAM_OUT $OUTPUT
+            # Ask user to abort testing.
+            if ! yesNoConfirm "Continue?"; then
                 exit 1
             fi
         fi
     fi
 
-    echo -n "Comparing stderr output... "
-    if diff my_out.err $ERR &> /dev/null; then
-        printGood "OK"
+    echo -n "Comparing stderr output...   "
+    if diff $PROGRAM_ERR $ERR &> /dev/null; then
+        echo -e "\e[1;32mOK\e[0m"
     else
-        printBad "DIFFERS!"
+        echo -e "\e[1;31mDIFFERS!\e[0m"
         if yesNoConfirm "Show diff?"; then
-            diff --color my_out.err $ERR
-            if yesNoConfirm "Abort testing?"; then
+            diff --color $PROGRAM_ERR $ERR
+            # Ask user to abort testing.
+            if ! yesNoConfirm "Continue?"; then
                 exit 1
             fi
         fi
@@ -97,17 +111,43 @@ for i in $(ls $DIRECTORY | egrep -i '*.in'); do
     # exit code.
     if $CHECK_FOR_MEM_LEAKS; then
         echo -n "Checking for memory leaks... "
-        if valgrind --leak-check=full --log-file="my_valgr.log" \
-                    --error-exitcode=1 $PROGRAM < $INPUT &> /dev/null; then
-            printGood "OK"
+
+        valgrind --leak-check=full --log-file="$PROGRAM_VALG" --error-exitcode=1 $PROGRAM < $INPUT &> /dev/null
+        VALGR_EXIT_CODE=$?
+
+        USAGE_INFO=`cat $PROGRAM_VALG | grep 'total heap usage'`
+        regex="==[0-9]+==\s+total heap usage: ([0-9,]+) allocs, ([0-9,]+) frees, ([0-9,]+) bytes allocated"
+        if [[ $USAGE_INFO =~ $regex ]]; then
+            ALLOCATED_SIZE=`echo ${BASH_REMATCH[3]} | tr --delete ,`
+            if [ $ALLOCATED_SIZE -ge $MAX_ALLOCATED_SIZE ];then
+                MAX_ALLOCATED_SIZE="$ALLOCATED_SIZE"
+                MAX_ALLOCATION_FILE="$INPUT"
+            fi
         else
-            printBad "LEAKS DETECTED!"
+            exit 2
+        fi
+
+        if [ "$VALGR_EXIT_CODE" -eq "0" ]; then
+            echo -ne "\e[1;32mOK\e[0m"
+            echo " (Total allocated: $ALLOCATED_SIZE B)"
+        else
+            echo -e "\e[1;31mLEAKS DETECTED!\e[0m"
             if yesNoConfirm "Show Valgrind output?"; then
-                cat my_valgr.log
-                if yesNoConfirm "Abort testing?"; then
+                cat $PROGRAM_VALG
+                if ! yesNoConfirm "Continue?"; then
                     exit 1
                 fi
             fi
         fi
     fi
 done
+
+# cleanup:
+rm $PROGRAM_OUT
+rm $PROGRAM_ERR
+rm $PROGRAM_VALG
+
+
+echo "Testing done."
+echo "Max time: $MAX_TIME_FILE ($MAX_TIME s)."
+echo "Max allocation at file: $MAX_ALLOCATION_FILE ($MAX_ALLOCATED_SIZE B)."
